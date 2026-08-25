f4821372845dc285fbefedac9cfd48df120e22be
Agileshkumar0506 <agileshdhananjeyan56@gmail.com>
2026-08-25 18:13:26 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,157 @@
+// 1. using variables we can declare the value.
+
+// 2. 1).var 2).let 3).const
+
+// 3. var name = 'Agilesh';
+
+// 4. let age = 22;
+
+// 5. const desigination = 'Full Stack Python Developer';
+
+
+//6.declaration   //7.initialization
+// var a = 10;
+    
+// 8.reassignment
+    // a = 20;
+
+//9.redeclaration
+
+// var a = 30;
+// console.log(a);
+
+// 10. var keyword allows redeclaration
+
+// 11. var,let keyword allows reassignment
+
+// 12. const keyword requires initialization when declared
+
+// 13. let age is declaration and 25 is initialization
+
+// 14. the value of a is 100
+
+// 15. let number = 200;
+
+// 16. 10 will be the output
+
+// 17. 20 is the output
+
+// 18. 30 is the output
+
+// 19. var name = 'john'
+
+// 20. var price = 500;
+
+// 21. var price = 500;
+//     var price = 1000;
+
+// 22. 100 is the output
+
+// 23. yes using var we can reassigned
+
+// 24. yes using var we can re-declaration
+
+// 25. 
+// var a = 10;
+// var a = 30;
+// console.log(a);
+
+// 26. let age = 25
+
+// 27. 30 is the output
+
+// 28. yes using let we can reassigned
+
+// 29. using let we cannot re-declared
+
+// 30. 
+// let name = "John";
+// let name = "David";
+// using let we cannot re-declaration
+
+// 31. let city = 'chennai'
+
+// 32. city = 'salem'
+
+// 33. 50 is the output
+
+// 34. let salary = 25000;
+
+// 35. salary = 30000;
+
+// 36. const pi = 3.14;
+
+// 37. const variable cannot be reassigned
+
+// 38. const variable cannot be redeclared
+
+// 39.
+// const age;
+// age = 25;
+// const variable cannot be reassigned
+
+// 40.
+// const price = 500;
+// price = 1000;
+// reassigning but it not possible in const
+
+// 41. const country = 'India';
+
+// 42. 100 is the output;
+
+// 43. const
+
+// 44. let
+//     we can reassign
+//     we cannot redeclaration
+
+//     const
+//     we can not do both reassign and redeclaration
+
+// 45. var 
+//     we can reassign
+//     we can redeclaration
+
+// const
+// we can not do both reassign and redeclaration
+
+// 46. console.log('HELLO WORLD');
+
+// 47. console.log(500)
+
+// 48 .using console.warn(); warn the user
+
+// 49. using console.error(); we can define the error
+
+// 50.
+// alert() - alert the user
+// prompt() - ask answer from user
+// confirm() - confirm the user like ask qusitions
+// document.writeln() - print the value in webpage
+// console.log() - print the value in console
+
+
+// Practical Questions
+
+
+// 1.
+// var name = 'Agilesh';
+// age = 22;
+// mark = 93;
+// console.log(name,age,mark)
+
+// 2.
+// const userName = prompt("Enter Ur Name");
+// alert(userName);
+
+// 3.
+// let userName = prompt("Enter Ur Name");
+// console.log(userName);
+
+// 4.
+// var Question = "Are you sure u want submit?";
+// confirm(queueMicrotask);
+
+// 5.
+// var userName = prompt("Enter Ur Name")
+// document.writeln(userName)
