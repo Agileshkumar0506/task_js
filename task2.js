ba287e15990c053047e45b267594dae5d31b0c6c
Agileshkumar0506 <agileshdhananjeyan56@gmail.com>
2026-09-01 10:13:23 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,213 @@
+// task-1
+
+// var studentName = "Agilesh";
+// let studentAge = 22;
+// const collegeName = "xxxx Engineering College";
+
+// console.log(studentName,studentAge,collegeName);
+// studentName = "kabilan";
+// console.log(studentName);
+// studentAge = 23;
+// console.log(studentAge);
+// collegeName = "xxx Engineering College";
+// console.log(collegeName);
+// var studentName = "Kabilan";
+// console.log(studentName);
+// let studentAge = 23;
+// console.log(studentAge);
+
+// task-2
+
+// var name = prompt("Enter Ur Name");
+// var age = prompt("Enter Ur Age");
+// var city = prompt("Enter Ur City");
+
+// console.log(name);
+// console.log(age);
+// console.log(city);
+
+
+// task-3
+
+// let name = prompt("Enter Ur Name");
+// alert("Welcome" +" "+ name +"!");
+
+// task-4
+
+// var birthYear = prompt("Enter Ur Birth Year");
+// var currentYear = 2026
+// console.log("Birth Year:" +" "+ birthYear);
+// console.log("Age:" +" "+ (currentYear - birthYear));
+
+// task-5
+
+// var a = "Hello"
+// let b = 100
+// const c = 25.5
+// var d = true
+// var e = false
+// var f = undefined
+// var g = null
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(f));
+// console.log(typeof(g));
+
+// task-6
+
+// var studentData = {
+//     name : 'Agilesh',
+//     age : 22,
+//     city : 'villupuram',
+//     qualification : "B.E",
+//     isStudent : true
+// };
+
+// console.log(studentData);
+// console.log(studentData.name);
+// console.log(studentData.age);
+// console.log(studentData.qualification);
+// console.log(studentData.isStudent);
+
+
+// task-7
+
+// var fruits = ["Apple","Mango","Orange","Banana","Grapes","Papaya"];
+
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[fruits.length-1]);
+// console.log(fruits.length);
+
+// task-8
+
+// let a = 20;
+// let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// task-9
+
+// var shirt = 999;
+// var pant = 1499;
+// var shoes = 1999;
+
+// console.log('Total:' +" "+ (shirt+pant+shoes));
+
+
+// task-10
+
+// var tamil = 80;
+// var english = 75;
+// var maths = 90;
+
+// console.log('Total:' +" "+ (tamil+english+maths));
+// console.log('Average Marks:' +" "+ ((tamil+english+maths) / 3));
+
+// task-11
+
+// let a = 10;  
+// let b = a++;
+
+// Expected output : a = 11, b = 10
+
+// console.log(a);
+// console.log(b);
+
+// task-12
+
+// let a = 10;
+// let b = ++a;
+
+// Expected output : a = 11, b = 11
+
+// console.log(a);
+// console.log(b);
+
+// task-13
+
+// let a = 20;
+// let b = a--;
+
+// Expected output : a = 19, b = 20
+
+// console.log(a);
+// console.log(b);
+
+// task-14
+
+// let a = 20;
+// let b = --a;
+
+// Expected output : a = 19, b = 19
+
+// console.log(a);
+// console.log(b);
+
+// task-15
+
+// let a = 5;
+// let b = a++;
+// let c = ++a;
+// let d = b--;
+
+// Expected output : a = 7, b = 4, c = 7 ,d = 5
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// task-16
+
+// let num = 10;
+// num += 5;
+// num -= 5;
+// num *= 5;
+// num /= 5;
+// num %= 5;
+// num **= 5;
+
+// console.log(num);
+
+// task-17
+
+// var studentData = {
+//     name : 'Agilesh',
+//     age : 22,
+//     city : 'villupuram',
+//     subjects : ['maths','science','social','tamil','english'],
+//     isStudent : true
+// };
+
+// console.log(studentData.name);
+// console.log(studentData.age);
+// console.log(studentData.city);
+// console.log(studentData.subjects[1]);
+// console.log(studentData.subjects[studentData.subjects.length-1]);
+// console.log(studentData.subjects.length);
+// console.log();studentData
+
+// Final Challenge
+
+// var value_1 = prompt("Enter A Value");
+// var value_2 = prompt("Enter B Value");
+
+// console.log(value_1+value_2);
+// console.log(value_1-value_2);
+// console.log(value_1*value_2);
+// console.log(value_1/value_2);
+// console.log(value_1%value_2);
+// console.log(value_1**value_2);
+
+
+
